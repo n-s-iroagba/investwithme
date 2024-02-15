fb18dec1eb8bb6a2963a42bbd9af4f8468240d09
n-s-iroagba <nnamdisolomon1@gmail.com>
2024-02-15 11:16:04 +0100

    initial commit

--- backend/AdminController.js
+++ backend/AdminController.js
@@ -1,45 +1,273 @@
 const Investor = require('../models/investor');
+const { Wallet } = require('../models');
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Destination directory for uploaded files
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname); // Rename the file with timestamp prefix
+  }
+});
 
-module.exports={
-     deleteInvestor:async (req, res) => {
-        try {
-          const investorId = req.params.id;
-          const investor = await Investor.findByPk(investorId);
-      
-          if (!investor) {
-            return res.status(404).json({ error: 'Investor not found' });
-          }
-      
-          await investor.destroy();
-      
-          res.status(200).json({ message: 'Investor deleted successfully' });
-        } catch (error) {
-          console.error('Error deleting investor:', error);
-          res.status(500).json({ error: 'An error occurred while deleting the investor' });
-        }
-      },
-      
-  addInvestmentAmount:async (req, res)=>{
+// Multer file upload configuration
+const upload = multer({ storage: storage });
+
+module.exports = {
+  addInvestmentAmount: async (req, res) => {
     try {
-      const { walletAddress, investmentAmount } = req.body;
-      const investor = await Investor.findOne({
-        where: {
-          depositingAddresses: {
-            [Op.contains]: [walletAddress]
+      const { walletAddress, investmentAmount, intendedManagerId } = req.body;
+      const wallet = await Wallet.findOne({ where: { address: walletAddress } });
+      if (!wallet) {
+        return res.status(404).json({ error: 'Wallet address not found' });
+      }
+      const investor = await Investor.findOne({ where: { id: wallet.investorId } });
+      if (!investor) {
+        return res.status(404).json({ error: 'Investor not found' });
+      }
+
+      let manager;
+      if (!investor.managerId) {
+        const newInvestorInvestmentAmount = 1.17 * investmentAmount;
+        manager = await Manager.findByPk(intendedManagerId);
+        if (!manager) {
+          throw new Error('the manager the investor chose is  not the database');
+        }
+        if (
+          newInvestorInvestmentAmount >= manager.minimumInvestmentAmount &&
+          newInvestorInvestmentAmount <= manager.maximumInvestmentAmount
+        ) {
+          investor.managerId = manager.id;
+          investor.incrementPercent = manager.incrementPercent;
+          await sendNewInvestorInvestmentMail(manager, investmentAmount, 0, investor, false);
+        } else {
+          const newManager = await Manager.findOne({
+            where: {
+              minimumInvestmentAmount: { [Op.lte]: newInvestorInvestmentAmount },
+              maximumInvestmentAmount: { [Op.gte]: newInvestorInvestmentAmount },
+            },
+            order: [['id', 'ASC']],
+          });
+          if (!newManager) {
+            investor.managerId = manager.id;
+          investor.incrementPercent = manager.incrementPercent;
+          }else{
+          manager = newManager
+          investor.managerId = manager.id;
+          investor.incrementPercent = manager.incrementPercent;
+          await sendNewInvestorInvestmentMail(manager, investmentAmount, 0, investor, true);
           }
         }
+        investor.investmentAmount = newInvestorInvestmentAmount;
+      } else {
+        const totalInvestmentValue = investor.investmentValue + investmentAmount;
+        manager = await Manager.findByPk(investor.managerId);
+        if (!manager) {
+          throw new Error('Current manager not found');
+        }
+        if (totalInvestmentValue > manager.maximumInvestmentAmount) {
+          const nextManager = await Manager.findOne({
+            where: {
+              minimumInvestmentAmount: { [Op.lte]: totalInvestmentValue },
+              maximumInvestmentAmount: { [Op.gte]: totalInvestmentValue },
+            },
+            order: [['minimumInvestmentAmount', 'ASC']],
+          });
+          if (nextManager) {
+            manager = nextManager;
+            investor.managerId = manager.id;
+            await sendNewInvestorInvestmentMail(manager, investmentAmount, investor.investmentAmount, investor, true);
+          } else {
+            await sendNewInvestorInvestmentMail(manager, investmentAmount, investor.investmentAmount, investor, false);
+          }
+        } else {
+          await sendNewInvestorInvestmentMail(manager, investmentAmount, investor.investmentAmount, investor, false);
+        }
+        investor.investmentValue = totalInvestmentValue;
+        investor.incrementPercent = 0.1 + manager.incrementPercent;
+      }
+      await investor.save();
+      const transaction = await Transaction.create({
+        investorId: investor.id,
+        amount: investmentAmount,
+        type: 'credit',
+        dateTime: new Date().toLocaleString('en-US', { timeZone: investor.timeZone }),
       });
+      await transaction.save()
+      return res.status(200).json({ message: 'Investment amount added successfully' });
+    } catch (error) {
+      console.error('Error adding investment amount:', error);
+      return res.status(500).json({ message: 'Error adding investment amount' });
+    }
+  },
+ getAllInvestors:async (req, res) => {
+    try {
+      const investors = await Investor.findAll();
+      return res.status(200).json({ investors: investors });
+    } catch (error) {
+      console.error('Error fetching investors:', error);
+      return res.status(500).json({ error: 'Error fetching investors' });
+    }
+  },
+  deleteInvestor: async (req, res) => {
+    try {
+      const investorId = req.params.id;
+      const investor = await Investor.findByPk(investorId);
 
       if (!investor) {
         return res.status(404).json({ error: 'Investor not found' });
       }
-      investor.investmentValue += investmentAmount;
-      await investor.save();
-      res.status(200).json({ message: 'Investment amount added successfully' });
+      await investor.destroy();
+      res.status(200).json({ message: 'Investor deleted successfully' });
     } catch (error) {
-      console.error('Error adding investment amount:', error);
-      res.status(500).json({ error: 'An error occurred while adding investment amount' });
+      console.error('Error deleting investor:', error);
+      res.status(500).json({ error: 'An error occurred while deleting the investor' });
     }
+  },
+
+
+
+  //Wallet
+  createAdminWallet:async (res,req) => {
+    const {address,type,currency} = req.body
+    const {id} = req.params
+      try {
+          // Create a new wallet instance
+          const wallet = await AdminWallet.create({
+              address: address,
+              type: type,
+              currency: currency
+              
+          });
+          wallet.save();
+          return res.status(200).json({message:`admin ${currency} wallet created`})
+      } catch (error) {
+          // Handle errors if any
+          console.error('Error creating wallet:', error);
+          throw error;
+      }
+  },
+ getAllWallets: async (req, res) => {
+    try {
+      const wallets = await Wallet.findAll();
+      return res.status(200).json({ wallets: wallets });
+    } catch (error) {
+      console.error('Error fetching wallets:', error);
+      return res.status(500).json({ error: 'Error fetching wallets' });
+    }
+  },
+  updateWalletAddressById: async (req, res) => {
+    const { id } = req.params; 
+    const { address } = req.body; 
+    try {
+      const wallet = await Wallet.findByPk(id);
+      if (!wallet) {
+        return res.status(404).json({ error: 'Wallet not found' });
+      }
+      wallet.address = address;
+      await wallet.save();
+      return res.status(200).json({ message: 'Wallet address updated successfully', wallet });
+    } catch (error) {
+      console.error('Error updating wallet address:', error);
+      return res.status(500).json({ error: 'Failed to update wallet address' });
+    }
+  },
+
+  deleteWalletAddress: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const wallet = await Wallet.findByPk(id);
+      if (!wallet) {
+        return res.status(404).json({ error: 'Wallet not found' });
+      }
+      await wallet.destroy();
+      return res.status(200).json({ message: 'Wallet address deleted successfully' });
+    } catch (error) {
+      console.error('Error deleting wallet address:', error);
+      return res.status(500).json({ error: 'Failed to delete wallet address' });
+    }
+  },
+
+
+
+//Manager
+ createManager :async (req, res) => {
+  try {
+    const { name, email, otherAttribute } = req.body;
+
+    let imageFilePath = null;
+    if (req.file) {
+      imageFilePath = req.file.path;
+    }
+
+    const manager = await Manager.create({
+      name: name,
+      email: email,
+      otherAttribute: otherAttribute,
+      image: imageFilePath,
+    });
+
+    return res.status(201).json({ message: 'Manager created successfully', manager: manager });
+  } catch (error) {
+    console.error('Error creating manager:', error);
+    return res.status(500).json({ error: 'Error creating manager' });
   }
+},
+getAllManagers : async (req, res) => {
+  try {
+    const managers = await Manager.findAll();
+    return res.status(200).json({ managers: managers });
+  } catch (error) {
+    console.error('Error fetching managers:', error);
+    return res.status(500).json({ error: 'Error getting  managers from database' });
+  }
+},
+updateManager : async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email, otherAttribute } = req.body;
+
+    let imageFilePath = null;
+    if (req.file) {
+
+      imageFilePath = req.file.path;
+    }
+    const manager = await Manager.findByPk(id);
+    if (!manager) {
+      return res.status(404).json({ error: 'Manager not found' });
+    }s
+    manager.name = name;
+    manager.email = email;
+    manager.otherAttribute = otherAttribute;
+    if (imageFilePath) {
+      manager.image = imageFilePath;
+    }
+    await manager.save();
+
+    return res.status(200).json({ message: 'Manager updated successfully' });
+  } catch (error) {
+    console.error('Error updating manager:', error);
+    return res.status(500).json({ error: 'Error updating manager' });
+  }
+},
+
+
+deleteManager: async (req, res) => {
+  try {
+      const { id } = req.params; 
+      const manager = await Manager.findByPk(id);
+
+      if (!manager) {
+          return res.status(404).json({ error: 'Manager does not exist in database' });
+      }
+
+      await manager.destroy();r
+
+      return res.status(200).json({ message: 'Manager deleted successfully' });
+  } catch (error) {
+      console.error('Error deleting manager:', error);
+      return res.status(500).json({ error: 'Error deleting manager' });
+  }
+
+},
 }
 
--- backend/Investorcontroller.js
+++ backend/Investorcontroller.js
@@ -10,30 +10,148 @@ module.exports = {
   },
   registerInvestor: async (req, res) => {
     try {
-      const { username, email, password, dateOfBirth } = req.body;
-      const newUser = await Investor.create({ username, email, password });
-
-      // Send verification email
-      await sendVerificationEmail(newUser);
-
-      res.status(201).json(newUser);
+      const { investorname, email, password, dateOfBirth, } = req.body;
+      const newinvestor = await Investor.create({ investorname, email, password });
+      await sendVerificationEmail(newinvestor);
+      res.status(201).json(newinvestor);
     } catch (error) {
-      console.error('Error registering user:', error);
+      console.error('Error registering investor:', error);
       res.status(400).json({ error: 'Could not register you at this point our servers are over loaded' });
     }
   },
+ updateInvestor:async (req, res) => {
+    try {
+      const { id } = req.params; // Extract the investor ID from req.params
+      const { firstName, lastName, email, phoneNumber } = req.body; // Extract updated attributes from req.body
+      let investor = await Investor.findByPk(id);
+  
+      if (!investor) {
+        return res.status(404).json({ error: 'Investor not found' });
+      }
+      investor.firstName = firstName;
+      investor.lastName = lastName;
+      investor.email = email;
+      investor.phoneNumber = phoneNumber;
+      await investor.save();
+  
+      return res.status(200).json({ message: 'Investor updated successfully', investor });
+    } catch (error) {
+      console.error('Error updating investor:', error);
+      return res.status(500).json({ error: 'Failed to update investor' });
+    }
+  },
+ uploadInvestorImage: async (req, res) => {
+    try {
+      const { id } = req.params; 
+      const investor = await Investor.findByPk(id); 
+  
+      if (!investor) {
+        return res.status(404).json({ error: 'Investor not found' });
+      }
+      if (!req.file) {
+        return res.status(400).json({ error: 'An image file must be provided' });
+      }
+      const imagePath = req.file.path;
+       investor.image = imagePath;
+      await investor.save();
+      return res.status(200).json({ message: 'Investor image uploaded successfully', imagePath: imagePath });
+    } catch (error) {
+      console.error('Error uploading investor image:', error);
+      return res.status(500).json({ error: 'Error uploading investor image' });
+    }
+  },
+  addWallet:async (res,req) => {
+    const {address,type,currency} = req.body
+    const {id} = req.params
+    if(type==='withdrawal'){
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+   address+= randomNumber.toString()
+    }
+      try {
+   
+          const wallet = await Wallet.create({
+              address: address,
+              type: type,
+              currency: currency,
+              investorid
+              
+          });
+          wallet.save();
+          return res.status(200).json({message:`admin ${currency} wallet created`})
+      } catch (error) {
+       
+          console.error('Error creating wallet:', error);
+          return res.status(500).json({message:`error adding wallet try again later`})
+      }
+  },
+  fetchInvestorWallets: async (req, res) => {
+    try {
+      const { investorId } = req.params;
+      const wallets = await Wallet.findAll({ where: { investorId: investorId } });
+      return res.status(200).json({ wallets: wallets });
+    } catch (error) {
+      console.error('Error fetching investor wallets:', error);
+      return res.status(500).json({ error: 'Error fetching investor wallets' });
+    }
+  },
+ updateWalletAddressById:async (req, res) => {
+    try {
+      const { id } = req.params; 
+      const { address } = req.body; 
+      let wallet = await Wallet.findOne({ where: { id: walletId, investorId: investorId } });
+  
+      if (!wallet) {
+        return res.status(404).json({ error: 'Wallet not found' });
+      }
+      wallet.address = address;
+      await wallet.save();
+      return res.status(200).json({ message: 'Wallet address updated successfully', wallet });
+    } catch (error) {
+      console.error('Error updating wallet address:', error);
+      return res.status(500).json({ error: 'Failed to update wallet address' });
+    }
+  },
+  deleteInvestorWalletAddress:async (req, res) => {
+    try {
+      const { investorId, walletId } = req.params;
+      const investor = await Investor.findByPk(investorId);
+  
+      if (!investor) {
+        return res.status(404).json({ error: 'Investor not found' });
+      }
+      const wallet = await Wallet.findOne({ where: { id: walletId, investorId: investorId } });
+  
+      if (!wallet) {
+        return res.status(404).json({ error: 'Wallet not found for this investor' });
+      }
+
+      await wallet.destroy();
+      return res.status(200).json({ message: 'Wallet address deleted successfully' });
+    } catch (error) {
+      console.error('Error deleting wallet address:', error);
+      return res.status(500).json({ error: 'Failed to delete wallet address' });
+    }
+  },
+  
+  
+
+
+
+
+
+
   login: async (req, res) => {
     try {
       const { email, password } = req.body;
-      const user = await Investor.findOne({ where: { email } });
-      if (!user) {
+      const investor = await Investor.findOne({ where: { email } });
+      if (!investor) {
         return res.status(401).json({ error: 'Invalid email or password' });
       }
-      const isPasswordValid = await bcrypt.compare(password, user.password);
+      const isPasswordValid = await bcrypt.compare(password, investor.password);
       if (!isPasswordValid) {
         return res.status(401).json({ error: 'Invalid email or password' });
       }
-      const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET);
+      const token = jwt.sign({ id: investor.id, email: investor.email }, JWT_SECRET);
       res.json({ token });
     } catch (error) {
       console.error('Error logging in:', error);
@@ -45,7 +163,7 @@ module.exports = {
     try {
       // GET INvestor
       if (investor.verified) {
-        console.log('User is already verified, no verification email needed.');
+        console.log('investor is already verified, no verification email needed.');
         return;
       }
       sendVerificationEmail(investor)
@@ -64,31 +182,86 @@ module.exports = {
        } 
        await investor.update({ verified: true, verificationToken: null });
         // Log the investor in
-        req.session.investor = investor
-        res.status(200).json({ message: 'Email verified successfully. You are now logged in.' });
-      
+        const jwToken = jwt.sign({ id: investor.id, email: investor.email }, JWT_SECRET);
+        res.status(200).json({ jwToken });    
     }catch (error) {
         console.error('Error verifying email:', error);
         res.status(500).json({ error: 'An error occurred while verifying your email. Please try again later.' });
       }
     },
-  }
+    forgotPassword: async (req, res) => {
+      try {
+        const { email } = req.body;
+        
+        // Generate a unique token
+        const token = crypto.randomBytes(20).toString('hex');
+    
+        // Find the investor by email
+        const investor = await Investor.findOne({ where: { email: email } });
+    
+        if (!investor) {
+          return res.status(404).send('investor not found');
+        }
+    
+        // Save the token and expiry time to the investor record
+        investor.resetPasswordToken = token;
+        investor.resetPasswordExpires = Date.now() + 3600000; // Token expires in 1 hour
+        await investor.save();
+        await sendChangePasswordTokenMail(investor)
+        res.send('Password reset instructions have been sent to your email.')
+      } catch (error) {
+        console.error('Error sending reset password email:', error);
+        res.status(500).send('Error sending reset password email');
+      }
+    },
+    checkPasswordToken: async (req, res) => {
+      try {
+        const token = req.params.token;
+        const user = await User.findOne({
+          where: {
+            resetPasswordToken: token,
+            resetPasswordExpires: { $gt: Date.now() }
+          }
+        });
+    
+        if (!user) {
+          return res.status(400).send('Password reset token is invalid or has expired.');
+        }
+    
+        // Redirect the user to a page where they can input their new password
+        res.redirect(`/reset-password-form?token=${token}`);
+      } catch (error) {
+        console.error('Error handling reset password request:', error);
+        res.status(500).send('Error handling reset password request');
+      }
 
-  
-
-const sendVerificationEmail = async (user) => {
+  },
+  resetPassword: async (req, res) => {
     try {
-      const verificationToken = generateVerificationToken();
-      await user.update({ verificationToken });
-
-      const verificationLink = `http://example.com/verify/${verificationToken}`;
-      const emailBody = `Please click this link to verify your email: ${verificationLink}`;
-
-      await EmailService.sendEmail(user.email, 'Verify Your Email', emailBody);
+      const { token, password } = req.body;
+      const user = await User.findOne({
+        where: {
+          resetPasswordToken: token,
+          resetPasswordExpires: { $gt: Date.now() }
+        }
+      });
+  
+      if (!user) {
+        return res.status(400).send('Password reset token is invalid or has expired.');
+      }
+  
+      // Reset the user's password
+      user.password = password;
+      user.resetPasswordToken = null;
+      user.resetPasswordExpires = null;
+      await user.save();
+  
+      res.send('Password has been successfully reset.');
     } catch (error) {
-      console.error('Error sending verification email:', error);
+      console.error('Error resetting password:', error);
+      res.status(500).send('Error resetting password.');
     }
   }
-const generateVerificationToken = () => {
-    return Math.random().toString(36).substring(7);
-  };
+
+} 
+
