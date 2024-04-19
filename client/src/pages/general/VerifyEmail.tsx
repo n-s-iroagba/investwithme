0b0011989ce04158855155ac2bc735ac428759fb
n-s-iroagba <nnamdisolomon1@gmail.com>
2024-04-19 03:11:22 +0100

    chore: removed unused vars

--- client/src/pages/general/VerifyEmail.tsx
+++ client/src/pages/general/VerifyEmail.tsx
@@ -49,8 +49,9 @@ const VerifyEmail = () => {
       setCounter((prevCounter) => Math.max(0, prevCounter - 1));
       const verificationStatus = localStorage.getItem('cassockVerified')
       if (verificationStatus){
+        alert('close')
         setCounter(0)
-        navigate('/already-verified')
+        window.close()
       }
     }, 1000);
 
