5a8019a9946abffc8425dd06ce3ccf8a79af3b4b
n-s-iroagba <nnamdisolomon1@gmail.com>
2024-07-13 23:47:25 +0100

    remove Investment type

--- client/src/features/investment/types/types.ts
+++ client/src/features/investment/types/types.ts
@@ -1,13 +1,13 @@
 import { Manager, ManagerDto } from "../../../../../common/managerType";
 import { WalletDto } from "../../../../../common/walletTypes";
-import { Investment } from "../../../../../server/src/types/investorTypes";
+// import { Investment } from "../../../../../server/src/types/investorTypes";
 
 export interface CreateInvestmentOptions{
     managers:ManagerDto[]
     wallets:WalletDto[]
 }
 export interface PortfolioDto{
-    investment:Investment,
+    investment:any,
     manager:Manager,
     referrals:{totalAmount:number,count:number}
   }
